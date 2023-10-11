9603238a29d7ab5eb73d432ffa6d7c29125ef945
Tatsuki5274 <yellow.sock1233@fastmail.com>
2023-10-11 15:21:42 +0900

    誤ったファイルを削除

--- task6-2/app/models/book.rb
+++
@@ -1,9 +0,0 @@
-class Book < ApplicationRecord
-  belongs_to :user
-  has_many :favorites
-  has_many :book_comments
-
-  validates :title,presence:true
-  validates :body,presence:true,length:{maximum:200}
-
-end
--- task6-2/app/models/book_comment.rb
+++
@@ -1,6 +0,0 @@
-class BookComment < ApplicationRecord
-    validates :comment, presence: true
-
-    belongs_to :book
-    belongs_to :user
-end
